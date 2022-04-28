cd345502d5ccaef0d28e958c1b86a65e6487b78b
alifaraj1401 <102616506+alifaraj1401@users.noreply.github.com>
2022-04-28 23:20:37 +0430

    Add files via upload

---
+++ ctapp/README.txt
@@ -0,0 +1,4 @@
+1-In this section, put the production data in the "test_train_production" section 
+  under the headings "appcttrainin" and "appcttrainout" in the "data_process" folder.
+  and  first run the "gen_train_all_bands" file and then the "get_to_train" file.
+2-Run the "train_net" file for training.
---
+++ ctapp/data_process/gen_train_all_bands.m
@@ -0,0 +1,62 @@
+clear;close all;
+
+savepath = 'pavia_trainappct.h5';
+size_input = 33;
+size_label = 21;
+scale = 2;
+load('appcttrainin.mat');
+    appctin=appcttrainin;
+load('appcttrainout.mat');
+appctout=appcttrainout;
+stride = 18;
+
+%% initialization
+dataa = zeros(size_input, size_input, 110, 1,1);
+label = zeros(size_label, size_label, 110, 1,1);
+padding = abs(size_input - size_label)/2;
+count = 0;
+
+
+tic
+
+    im_input = im2double(appctin(:, :, :));
+    image_true=appctout(:, :, :);
+    [hei,wid,l] = size(im_input);
+   % disp(i)
+    for x = 1 : stride : hei-size_input+1
+        disp(x )
+        for y = 1 :stride : wid-size_input+1
+            
+            subim_input = im_input(x : x+size_input-1, y : y+size_input-1,:);
+            subim_label = image_true(x+padding : x+padding+size_label-1, y+padding : y+padding+size_label-1,:);
+
+            count=count+1;
+            dataa(:, :, :,1, count) = subim_input;
+            label(:, :, :, 1,count) = subim_label;
+        end
+    end
+
+size(dataa)
+size(label)
+order = randperm(count);
+dataa = dataa(:, :, :, order);
+label = label(:, :, :, order); 
+%% writing to HDF5
+chunksz = 128;
+created_flag = false;
+totalct = 0;
+
+for batchno = 1:floor(count/chunksz)
+    last_read=(batchno-1)*chunksz;
+    batchdata = dataa(:,:,:,last_read+1:last_read+chunksz); 
+    batchlabs = label(:,:,:,last_read+1:last_read+chunksz);
+    disp([batchno, floor(count/chunksz)])
+    startloc = struct('dat',[1,1,1,totalct+1], 'lab', [1,1,1,totalct+1]);
+    curr_dat_sz = store2hdf5(savepath, batchdata, batchlabs, ~created_flag, startloc, chunksz); 
+    created_flag = true;
+    totalct = curr_dat_sz(end);
+end
+toc
+
+h5disp(savepath);
+
